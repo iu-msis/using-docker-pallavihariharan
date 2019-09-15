a738b500c62fc0c9ebfd18532ca8b7c1a79928bd
Chitra Pallavi Hariharan <cph1234@gmail.com>
2019-09-15 19:43:48 -0400

    Added js file

---
+++ app/public/js/user.app.js
@@ -0,0 +1,41 @@
+ /*jshint esversion: 6 */
+var userApp = new Vue({
+  el: '#userData',
+  data: {
+    users: {
+      name:{
+        title:'',
+        last:'',
+        first:''
+      },
+      email:'',
+      dob:{
+        date:'',
+        age:''
+      },
+      picture:{
+        Thumbnail:''
+      },
+      NAT:''
+    }
+  },
+  methods: {
+    fetchUserData() {
+    //  fetch('dummy.php')
+      //  .then(response => response.json() )
+        //.then(json => {waitingApp.patients = json});
+
+      // Means the same at this
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json() )
+      .then(json => {userApp.users = json.results[0]});
+
+        //.then(function(json) {waitingApp.people = json});
+
+    }
+  },
+  created() {
+    this.fetchUserData();
+  }
+
+});
